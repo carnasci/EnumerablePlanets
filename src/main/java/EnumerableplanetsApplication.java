4cae327bbdaa76a3c11ac2d3bb7931544c4188fa
carnasci <cen.master24@gmail.com>
2024-01-25 16:07:42 -0600

    finished chap 16 exercises but have import issues

--- src/main/java/EnumerableplanetsApplication.java
+++ src/main/java/EnumerableplanetsApplication.java
@@ -1,5 +1,3 @@
-package org.launchcode.enumerableplanets;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/org/launchcode/controllers/PlanetController.java
+++ src/main/java/org/launchcode/controllers/PlanetController.java
@@ -1,13 +1,16 @@
 package org.launchcode.controllers;
 
+import org.launchcode.Data.Planets;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PlanetController {
 
     @GetMapping
-    public String displayIndex() {
+    public String displayIndex(Model model) {
+        model.addAttribute("planets", Planets.values());
         return "index";
     }
 }
